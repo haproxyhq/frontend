5db75c53bb75f2541155caf09126047ae043f635
Maximilian Büttner <maximilian.buettner@westernacher.com>
2015-12-26 20:13:48 +0100

    added routing and components for home and about

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -1,10 +1,20 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {HomeComponent} from '../home/home.component'
+import {AboutComponent} from '../about/about.component'
 
 @Component({
   selector: 'app',
   templateUrl: './app/components/app/app.component.html',
-  styleUrls: ['./app/components/app/app.component.css']
+  styleUrls: ['./app/components/app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
+
+@RouteConfig([
+  { path:'/about', name: 'About',  component: AboutComponent },
+  { path:'/',      name: 'Home',   component: HomeComponent }
+])
 
 export class AppComponent {
 
