362ea1ba148dae2b9f68217a9cac0592adbde563
Maximilian Büttner <maximilian.buettner@westernacher.com>
2015-12-26 19:52:15 +0100

    renamed app component files to match naming convention

---
+++ app/components/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app/components/app/app.component.html',
+  styleUrls: ['./app/components/app/app.component.css']
+})
+
+export class AppComponent {
+
+}
