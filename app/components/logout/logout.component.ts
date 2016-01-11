852498181968e09b68da2e410c5006ef959cb7bf
Maximilian Büttner <maximilian.buettner@westernacher.com>
2016-01-11 16:25:30 +0100

    added toast for failing logout;

--- app/components/logout/logout.component.ts
+++ app/components/logout/logout.component.ts
@@ -1,7 +1,11 @@
 import {Component}              from 'angular2/core';
 import {Router}                 from 'angular2/router';
 
-import {AuthenticationService}   from '../../services/general/authentication.service';
+import {AuthenticationService}  from '../../services/general/authentication.service';
+
+import {ToastModel}             from '../../models/toast.model';
+
+declare var $;
 
 @Component({
   selector: 'logout',
@@ -11,7 +15,9 @@ import {AuthenticationService}   from '../../services/general/authentication.ser
 export class LogoutComponent {
 
   constructor(private _authenticationService: AuthenticationService, private _router: Router) {
-    if (this._authenticationService.logout()) this._router.navigate(['Login']);
+    if (this._authenticationService.logout()) {
+      this._router.navigate(['Login']);
+    } else $.snackbar(new ToastModel('Logout failed', '', 3000, true));
   }
 
 }
