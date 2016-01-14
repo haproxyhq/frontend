9b5ae114a4fe847799bc70d440aba09404699fbb
Maximilian Büttner <maximilian.buettner@westernacher.com>
2016-01-15 00:19:17 +0100

    added optional autocomplete to input-field;

--- app/components/general/input-field.component.ts
+++ app/components/general/input-field.component.ts
@@ -1,20 +1,29 @@
 import {Component, Input, Output, EventEmitter} from 'angular2/core';
 
+import {CompletionComponent}                    from '../completion/completion.component';
+import {Completion}                             from '../../models/wrapper/completion.model';
+
 @Component({
   selector: 'input-field',
   templateUrl: './components/general/input-field.component.html',
-  styleUrls: ['./components/general/input-field.component.css']
+  styleUrls: ['./components/general/input-field.component.css'],
+  directives: [CompletionComponent]
 })
 
 export class InputFieldComponent {
   @Input() label: string;
   @Input() value: string;
   @Input() required: boolean = false;
+  @Input('completion-data') completion: Completion;
   @Output() valueChange = new EventEmitter();
 
   constructor() {}
 
   updateValue(value) {
     this.valueChange.next(value);
+  }
+
+  showCompletion(): boolean {
+    return this.completion !== undefined && this.completion !== null;
   }
 }
