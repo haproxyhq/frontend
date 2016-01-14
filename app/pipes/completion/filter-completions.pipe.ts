0e02e908ba1f74c818de1536d279bea29d50c79b
Maximilian Büttner <maximilian.buettner@westernacher.com>
2016-01-15 00:17:52 +0100

    added pipe for processing Completion searches;

---
+++ app/pipes/completion/filter-completions.pipe.ts
@@ -0,0 +1,33 @@
+import {Pipe, PipeTransform}  from 'angular2/core';
+
+import {CompletionSection}    from '../../models/wrapper/completion-section.model';
+
+/**
+ * filters the completions data keywords
+ * Usage:
+ *   completions | filterCompletions:text
+ * Example:
+ *   {{ Completion.data |  filterCompletions:"abc"}}
+ *   formats to: all items that start with abc
+**/
+@Pipe({name: 'filterCompletions'})
+export class FilterCompletionsPipe implements PipeTransform {
+  transform(values: Array<CompletionSection>, args:string[]) : any {
+    return values.filter((item: CompletionSection) => {
+      let keywordStartsWith: boolean = item.keyword.startsWith(args[0]);
+      let searchStartsWith: boolean = args[0].startsWith(item.keyword);
+
+      if (keywordStartsWith) {
+        item.highlight = item.keyword.substr(0, args[0].length);
+        item.normal = item.keyword.substr(args[0].length);
+      } else if (searchStartsWith) {
+        item.highlight = item.keyword;
+        item.normal = '';
+      } else {
+        item.highlight = item.normal = '';
+      }
+
+      return keywordStartsWith || searchStartsWith;
+    });
+  }
+}
