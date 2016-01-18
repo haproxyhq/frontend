ff50aed6a74e9423869d0a87c88ed8015b1771d5
Maximilian Büttner <maximilian.buettner@westernacher.com>
2016-01-18 01:09:29 +0100

    using key-inputs for navigating in the completion list;

--- app/components/completion/completion.component.ts
+++ app/components/completion/completion.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, EventEmitter, Output} from 'angular2/core';
+import {Component, Input, EventEmitter, Output, OnInit} from 'angular2/core';
 
 import {Completion}                             from '../../models/wrapper/completion.model';
 import {CompletionSection}                      from '../../models/wrapper/completion-section.model';
@@ -12,10 +12,44 @@ import {FilterCompletionsPipe}                  from '../../pipes/completion/fil
   pipes: [FilterCompletionsPipe]
 })
 
-export class CompletionComponent {
+export class CompletionComponent implements OnInit {
   @Input() completion: Completion;
   @Input() search: string;
+  @Input('key-code') keyCode: EventEmitter<number>;
   @Output('selected-value') selectedValueEvent: EventEmitter<String> = new EventEmitter();
+
+  private _selected: number = -1;
+  private _filterCompletionsPipe: FilterCompletionsPipe = new FilterCompletionsPipe();
+  private _filtered;
+
+  ngOnInit() {
+    this._filtered = this._filterCompletionsPipe.transform(this.completion.data, [this.search]);
+
+    this.keyCode.subscribe(
+      (code) => {
+        this._filtered = this._filterCompletionsPipe.transform(this.completion.data, [this.search]);
+
+        if (code === 38) {
+          if (this._selected > -1) {
+            this._selected = this._selected - 1;
+          }
+        } else if (code === 40) {
+          if (this._selected === this._filtered.length - 1) {
+            this._selected = 0;
+          } else this._selected = this._selected + 1;
+        } else if (code === 13) {
+          if (this._selected !== -1 && this._selected < this._filtered.length) {
+            this.selectedValueEvent.next(this._filtered[this._selected].keyword);
+            this.search = this._filtered[this._selected].keyword;
+            this._filtered = this._filterCompletionsPipe.transform(this.completion.data, [this.search]);
+            this._selected = -1;
+          }
+        } else {
+          this._selected = -1;
+        }
+      }
+    );
+  }
 
   constructor() {}
 
