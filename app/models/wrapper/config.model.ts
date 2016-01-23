6febeec6e1fb0900083dc0faa431b4c3c34eb727
Maximilian Büttner <maximilian.buettner@westernacher.com>
2016-01-23 15:49:10 +0100

    updated config model;

--- app/models/wrapper/config.model.ts
+++ app/models/wrapper/config.model.ts
@@ -1,11 +1,21 @@
-import {ConfigSection}  from './config-section.model';
+import {RestWrapperModel}     from './rest-wrapper.model';
+import {ConfigSection}        from './config-section.model';
 
-export class Config {
+export class Config extends RestWrapperModel {
   public section: ConfigSection;
   public values: Array<String> = [];
 
   constructor(plainObject: any) {
+    super();
+    this.transformPlainObject(plainObject);
+  }
+
+  protected transformPlainObject(plainObject :any): void {
     this.section = new ConfigSection(plainObject.section.name, plainObject.section.type);
     this.values = plainObject.values;
+  }
+
+  public getRestModel(): Object {
+    return this._transformToPOJO('section', 'valus');
   }
 }
