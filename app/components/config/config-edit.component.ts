30e347ee73886efabe9d6a5969bac5fd04763bc8
Maximilian Büttner <maximilian.buettner@westernacher.com>
2016-01-23 22:45:18 +0100

    added config-edit component;

---
+++ app/components/config/config-edit.component.ts
@@ -0,0 +1,90 @@
+import {Component,ChangeDetectorRef, ChangeDetectionStrategy, Input, Output, OnInit, EventEmitter}  from 'angular2/core';
+
+import {ConfigSectionComponent}       from './config-section.component';
+import {InputFieldComponent}          from '../general/input-field.component';
+
+import {GlobalStorageService}         from '../../services/general/global-storage.service';
+import {CompletionService}            from '../../services/completion/completion.service';
+
+import {ProtectedDirective}           from '../../directives/general/protected.directive';
+import {UiSortableComponent}          from '../../directives/general/ui-sortable.directive';
+
+import {Completion}                   from '../../models/wrapper/completion.model';
+import {Config}                       from '../../models/wrapper/config.model';
+import {ConfigSection}                from '../../models/wrapper/config-section.model';
+
+@Component({
+  selector: 'config-edit',
+  changeDetection: ChangeDetectionStrategy.Default,
+  templateUrl: './components/config/config-edit.component.html',
+  styleUrls: ['./components/config/config-edit.component.css'],
+  directives: [ProtectedDirective, UiSortableComponent, ConfigSectionComponent]
+})
+
+export class ConfigEditComponent implements OnInit {
+  @Input() completion: Completion;
+  @Input() types: Array<string>;
+  @Input() config: Config;
+  @Output() configChange = new EventEmitter();
+
+  private _sectionHelper: Array<number> = [];
+  private _sections = {};
+
+  private _sortableOptions: Object;
+
+  constructor(private _ref: ChangeDetectorRef) {
+    let that = this;
+    this._sortableOptions = {
+      /**
+      * the callback stop function for the sortable action
+      * @param e the event fired
+      * @param s the sortable object
+      **/
+      stop: function(e, s): void {
+        that.configChange.next(that._transformToConfig());
+        that._ref.detectChanges();
+      }
+    };
+  }
+
+  ngOnInit() {
+    this._parseConfig();
+  }
+
+  /**
+  * react to a value change in an input
+  **/
+  private _sectionChange(key, value) {
+    this._sections[key] = value;
+    this._ref.detectChanges();
+    this.configChange.next(this._transformToConfig());
+  }
+
+  /**
+  * transforms the plain object to an config object
+  * @return returns a new Config instance
+  **/
+  private _transformToConfig(): Config {
+    let plainConfig = {
+      config: []
+    };
+    this._sectionHelper.forEach((key) => {
+      if (this._sections[key] !== null) {
+        plainConfig.config.push(this._sections[key]);
+      }
+    });
+    return new Config(plainConfig);
+  }
+
+  /**
+  * parses the @Input() field to the internal structure
+  **/
+  private _parseConfig() {
+    if (this.config !== null) {
+      this.config.config.forEach((section, index, array) => {
+        this._sectionHelper.push(index);
+        this._sections[index] = section;
+      });
+    }
+  }
+}
