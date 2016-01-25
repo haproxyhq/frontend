28f8c972ae60da47453edfe28c55e805965a3973
Jonas Depoix <jdepoix@seibert-media.net>
2016-01-25 23:02:42 +0100

    schema model added

---
+++ app/models/wrapper/schema.mode.ts
@@ -0,0 +1,23 @@
+import {RestWrapperModel} from './rest-wrapper.model';
+import {Config} from './config.model';
+
+export class Schema extends RestWrapperModel {
+  private name: string;
+  private description: string;
+  private configHolder: Config;
+
+  public constructor(plainObject: any) {
+    this.transformPlainObject(plainObject);
+  }
+
+  getRestModel(): Object {
+    return this._transformToPOJO('name', 'description', 'configHolder');
+  }
+
+  protected transformPlainObject(plainObject: any): void {
+    if (plainObject.configHolder !== null && plainObject.configHolder !== undefined) {
+      this.configHolder = new Config(plainObject.configHolder);
+    }
+    this._transformFromPOJO(plainObject, 'id', 'name', 'description');
+  }
+}
