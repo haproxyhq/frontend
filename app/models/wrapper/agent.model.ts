b6e89dd6e610f4c3f9b02e68db22dcfc53ea5c4f
Maximilian Büttner <maximilian.buettner@westernacher.com>
2016-01-25 01:06:34 +0100

    added undefined check; added id as field;

--- app/models/wrapper/agent.model.ts
+++ app/models/wrapper/agent.model.ts
@@ -5,11 +5,12 @@ import {Config}           from './config.model';
  * this model wraps the data for a Agent returned by the backend
  */
 export class Agent extends RestWrapperModel {
+  public id: string;
   public name: string;
   public description: string;
   public ip: string;
   public version: string;
-  public configHolder: Config;
+  public configHolder: Config = null;
 
   constructor(plainObject: any) {
     super();
@@ -18,8 +19,10 @@ export class Agent extends RestWrapperModel {
 
   protected transformPlainObject(plainObject :any): void {
     if (Object.keys(plainObject).length) {
-      if (plainObject.configHolder != null) this.configHolder = new Config(plainObject.configHolder);
-      this._transformFromPOJO(plainObject, 'name', 'description', 'ip', 'version');
+      if (plainObject.configHolder !== null && plainObject.configHolder !== undefined) {
+        this.configHolder = new Config(plainObject.configHolder);
+      }
+      this._transformFromPOJO(plainObject, 'id', 'name', 'description', 'ip', 'version');
     }
   }
 
