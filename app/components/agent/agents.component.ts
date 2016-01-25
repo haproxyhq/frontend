decc616d19753bb53c182d952d08ccc14a75a0fa
Jonas Depoix <jdepoix@seibert-media.net>
2016-01-25 23:44:34 +0100

    agents component loads schemas using schema service

--- app/components/agent/agents.component.ts
+++ app/components/agent/agents.component.ts
@@ -7,9 +7,11 @@ import {AgentDetailComponent}         from './agent-detail.component';
 
 import {GlobalStorageService}         from '../../services/general/global-storage.service';
 import {AgentService}                 from '../../services/agent/agent.service';
+import {SchemaService}                from '../../services/schema/schema.service';
 
 import {Agent}                        from '../../models/wrapper/agent.model';
 import {ToastModel}                   from '../../models/toast.model';
+import {Schema}                       from '../../models/wrapper/schema.model';
 
 declare var $;
 
@@ -22,10 +24,15 @@ declare var $;
 
 export class AgentsComponent implements OnInit {
   public agents: Array<Agent> = [];
+  public schemas: Array<Schema> = [];
   public agentsLoaded: boolean = false;
+  public schemasLoaded: boolean = false;
   public newAgent: Agent = new Agent({});
 
-  public constructor(private _globalStorage: GlobalStorageService, private _agentService: AgentService, private _router: Router) {}
+  public constructor(private _globalStorage: GlobalStorageService,
+                     private _agentService: AgentService,
+                     private _schemaService: SchemaService,
+                     private _router: Router) {}
 
   public ngOnInit(): void {
     this._agentService.getAgents().subscribe((agents) => {
@@ -35,6 +42,16 @@ export class AgentsComponent implements OnInit {
         this.agentsLoaded = true;
       } else {
         $.snackbar(new ToastModel('Loading Agents failed'));
+      }
+    });
+    this._schemaService.getSchemas().subscribe((schemas) => {
+      this.schemas = schemas;
+      this._globalStorage.schemas = this.schemas;
+      console.log(this._globalStorage.schemas);
+      if (this.schemas !== null) {
+        this.schemasLoaded = true;
+      } else {
+        $.snackbar(new ToastModel('Loading Schemas failed'));
       }
     });
   }
