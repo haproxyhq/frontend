01971d27298ddacee4137cf7a3e281bf5ae32416
Jonas Depoix <jdepoix@seibert-media.net>
2016-01-28 11:36:45 +0100

    seperate component for user detail added

---
+++ app/components/admin/user-detail.component.ts
@@ -0,0 +1,13 @@
+import {Component}   from 'angular2/core';
+import {Input} from 'angular2/core';
+
+import {User} from '../../models/wrapper/user.model';
+
+@Component({
+  selector: 'user-detail',
+  templateUrl: './components/admin/user-detail.component.html',
+  styleUrls: ['./components/admin/user-detail.component.css']
+})
+export class UserDetailComponent {
+  @Input() user: User;
+}
