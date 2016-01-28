8f6807ac0bbfb7c39991092479de63d80434cb5c
Jonas Depoix <jdepoix@seibert-media.net>
2016-01-28 11:55:47 +0100

    getUsers() added to UserService

--- app/services/user/user.service.ts
+++ app/services/user/user.service.ts
@@ -4,12 +4,22 @@ import {Response}                 from 'angular2/http';
 import {CustomHttpService}        from '../../services/general/custom-http.service';
 
 import {User}                     from '../../models/wrapper/user.model';
+import {EmptyRestModel} from '../../models/empty-rest.model';
 
+/**
+ * this service takes care of all CRUD operations regarding users
+ */
 @Injectable()
 export class UserService {
 
   constructor(private _http: CustomHttpService) {}
 
+  /**
+   * returns one specific user identified by email
+   *
+   * @param email the users email
+   * @returns {EventEmitter<User>}
+   */
   public getUser(email: string): EventEmitter<User> {
     var event: EventEmitter<User> = new EventEmitter();
 
@@ -28,6 +38,41 @@ export class UserService {
     return event;
   }
 
+  /**
+   * gets all users
+   *
+   * @returns {EventEmitter<Array<User>>}
+   */
+  public getUsers(): EventEmitter<Array<User>> {
+    var event: EventEmitter<Array<User>> = new EventEmitter();
+
+    this._http.get('http://localhost:8080/users/')
+      .map((res: Response) => res.json())
+      .subscribe(
+        (res) => {
+          let users: Array<User> = [];
+          if(res.content && !(EmptyRestModel.instanceOf(res.content[0]))) {
+            res.content.forEach((elem, index, array) => {
+              users.push(new User(elem));
+            });
+          }
+          event.emit(users);
+        },
+        (err) => {
+          event.emit(null);
+        },
+        () => {}
+      );
+
+    return event;
+  }
+
+  /**
+   * saves all changes made to a user
+   *
+   * @param user the changed user
+   * @returns {EventEmitter<User>}
+   */
   public saveUser(user: User): EventEmitter<User> {
     let event: EventEmitter<User> = new EventEmitter<User>();
 
@@ -35,10 +80,10 @@ export class UserService {
       .map((res: Response) => res.json())
       .subscribe(
         (res) => {
-          event.next(new User(res));
+          event.emit(new User(res));
         },
         (err) => {
-          event.next(null);
+          event.emit(null);
         },
         () => {}
     );
