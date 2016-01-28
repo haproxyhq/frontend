275bdf40c14c240b4fa35c7a638f4d85141ca5f9
Jonas Depoix <jdepoix@seibert-media.net>
2016-01-28 12:12:55 +0100

    animation moved to child view

--- app/components/admin/user-detail.component.ts
+++ app/components/admin/user-detail.component.ts
@@ -3,10 +3,14 @@ import {Input} from 'angular2/core';
 
 import {User} from '../../models/wrapper/user.model';
 
+import {SaveCancelFabsComponent} from '../general/save-cancel-fabs.component';
+import {InputFieldComponent} from '../general/input-field.component';
+
 @Component({
   selector: 'user-detail',
   templateUrl: './components/admin/user-detail.component.html',
-  styleUrls: ['./components/admin/user-detail.component.css']
+  styleUrls: ['./components/admin/user-detail.component.css'],
+  directives: [InputFieldComponent, SaveCancelFabsComponent]
 })
 export class UserDetailComponent {
   @Input() user: User;
