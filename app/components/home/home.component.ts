428f4afb3ed06b68d2e353374637691a2da8772a
Jonas Depoix <jdepoix@seibert-media.net>
2016-01-31 22:24:38 +0100

    home view updated

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -1,22 +1,17 @@
 import {Component}            from 'angular2/core';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {OnInit} from 'angular2/core';
 
 import {GlobalStorageService} from '../../services/general/global-storage.service';
 import {User} from '../../models/wrapper/user.model';
-import {OnInit} from 'angular2/core';
 
 @Component({
   selector: 'home',
   templateUrl: './components/home/home.component.html',
-  styleUrls: ['./components/home/home.component.css']
+  styleUrls: ['./components/home/home.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 
-export class HomeComponent implements OnInit {
-  private _user: User;
-  private _animationSequences: Array<boolean> = [];
-
+export class HomeComponent {
   constructor(private _globalStorage: GlobalStorageService) {}
-
-  ngOnInit(): any {
-    this._user = this._globalStorage.user;
-  }
 }
